cacdb4d8091587ab110f73fa1cab30f336b1641f
nicksharp117 <49224000+nicksharp117@users.noreply.github.com>
2019-05-27 19:39:40 -0700

    Nick Added this
    YA fun stuff

--- interface.js
+++ interface.js
@@ -7,16 +7,25 @@ var messages = {
 		warning: 'You do not have any time for mistakes--<br>not even for one wrong turn!!!',
 		ready: 'Are you ready?',
 		begin: 'Instructions:<br><br>Select one of the buttons on the right that describes the COLOR of the text on the left!',
+		tutwrong: 'Wrong button, try again!',
 		wrong: 'You have taken the wrong road and died :(',
+		tutright: 'Correct! Looks like you are ready to go.',
 		right: 'This appears to be the correct path.<br>The journey continues...'};
 var colors = {
 	pink: 'PINK',
 	blue: 'BLUE',
 	grey: 'GREY',
 	orange: 'ORANGE',
-	red: 'RED',
-	green: 'GREEN',
-	purple: 'PURPLE'};
+	green: 'GREEN'};
+	
+var butnum;
+var curButton = ["child1", "child2","child3","child4","child5"];
+
+var textColor = ["#ff99aa", "#0000e6", "#666666", "#e69500", "#00cc00"];
+var textsent = ["PINK", "BLUE", "GREY", "ORANGE", "GREEN"];
+	
+//textColor[0] = ""#ff99aa"
+	
 function dialogue() {
 	clickNum++;
 	switch (clickNum) {
@@ -39,43 +48,82 @@ function dialogue() {
 			document.getElementById('bg').className = "tutorial-bg";
 			document.getElementById('parent').className = "tutorial-parent";
 			document.getElementById('child').className = "tutorial-child";
+			myButt.disabled = true;
 			var i;
 			for (i = 1; i < 6; i++) { 
 				switch (i) {
 					case 1:
 						butt1 = document.createElement("BUTTON");
+						butt1.id = "butt1";
 						butt1.innerHTML = "PINK";
+						butt1.onclick = tutcorrect;
 						document.getElementById('child1').appendChild(butt1);
 						break;
 					case 2:
 						butt2 = document.createElement("BUTTON");
+						butt2.id = "butt2";
 						butt2.innerHTML = "BLUE";
+						butt2.onclick = tutwrong;
 						document.getElementById('child2').appendChild(butt2);
 						break;
 					case 3:
 						butt3 = document.createElement("BUTTON");
+						butt3.id = "butt3";
 						butt3.innerHTML = "GREEN";
+						butt3.onclick = tutwrong;
 						document.getElementById('child3').appendChild(butt3);
 						break;
 					case 4:
 						butt4 = document.createElement("BUTTON");
+						butt4.id = "butt4";
 						butt4.innerHTML = "GREY";
+						butt4.onclick = tutwrong;
 						document.getElementById('child4').appendChild(butt4);
 						break;
 					case 5:
 						butt5 = document.createElement("BUTTON");
+						butt5.id = "butt5";
 						butt5.innerHTML = "ORANGE";
+						butt5.onclick = tutwrong;
 						document.getElementById('child5').appendChild(butt5);
 						break;
 				}
 			}
-			word.innerHTML = colors.purple;
+			word.innerHTML = colors.blue;
 			break;
+		case 6:
+			
+			document.getElementById("myButt").onclick = curTime();
+			clickNum++;
+			break;
+		
+		default:
+			document.getElementById("butt1").onclick = curTime();
+			break;
+			
 	}
+}
+function tutcorrect() {
+	text.innerHTML = messages.tutright;
+	myButt.disabled = false;
+}
+function tutwrong() {
+	text.innerHTML = messages.tutwrong;
 }
 function correct() {
 	text.innerHTML = messages.right;
 }
 function wrong() {
 	text.innerHTML = messages.wrong;
+}
+
+function curTime() {
+	var random = Math.floor(Math.random()*5);
+	var random2 = Math.floor(Math.random()*5);
+	//var num2 = rand.nextInt(7);
+	butnum = random;
+	word.innerHTML = textsent[random2];
+	word.style.color = textColor[0];
+	
+	//return System.currentTimeMillis();
 }
