46f55b3ca2d33a0a66ebe9ea3e5ad6e8e60ccc02
eokuna <48928032+eokuna@users.noreply.github.com>
2019-05-28 19:41:00 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/',
+	function (req, res) {
+		res.sendFile(__dirname + "/public/interface.html");
+		console.log("Introducing the game.");
+});
+
+app.get('/play',
+	function(req, res) {
+		res.sendFile(__dirname + '/public/play.html');
+		console.log("User playing now.");
+});
+
+app.get('/lose',
+	function(req, res) {
+		res.sendFile(__dirname + '/public/player-lost.html');
+		console.log("The player has lost.");
+});
+
+app.get('/win',
+	function(req, res) {
+		res.sendFile(__dirname + '/public/player-won.html');
+		console.log("The player has won.");
+});
+
+var server = app.listen(3024,
+	function () {
+		var host = server.address().address
+		var port = server.address().port
+		console.log("Stroop app listening at http://" + host + ":" + port);
+});
